1b14668b6bfbbbe96e9666fa7849b3d325a05923
gigih santoso <gigih985042@gmail.com>
2024-12-19 14:51:55 +0700

    modified:   src/app/app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     {
         path: '',
-        redirectTo: 'natural',
+        redirectTo: 'formula',
         pathMatch: 'full'
     },
     {
